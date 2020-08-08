7e3dd908b8bfc50737a9828e184d1cc87e0bf7f3
mrcreatist <ahk.verma102@gmail.com>
2020-08-08 12:17:17 +0530

    updated prod ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  socketURL: 'ws://192.168.1.201:3334',
-  url: 'http://192.168.1.201:3334',
+  socketURL: 'ws://192.168.1.200:3334',
+  url: 'http://192.168.1.200:3334',
   production: true
 };
